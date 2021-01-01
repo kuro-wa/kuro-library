30627612fac09acd3961e9f5b40698a1342a7cfa
letterpair <hellok-taka@docomo.ne.jp>
2021-01-01 22:53:54 +0900

    first commit

---
+++ document/smallmodfactorial.md
@@ -0,0 +1,56 @@
+[TOC]
+
+# Small Mod Factorial
+
+階乗を含む式の計算をmodを取って行う構造体です．
+
+[Mod Factorial](modfactorial.md)とは異なり，modの値が小さく，式の中の値が大きいときに使います．
+
+- 問題例: $0 \leq k \leq n \leq 10^{18}$ に対して，$_n C_k$ の偶奇を判定せよ．
+
+## コンストラクタ
+
+```cpp
+SmallModFactoriall<modint> d()
+```
+
+構造体を初期化します．`modint` で答えを計算します．以下，$p$ を `modint` の法とします．
+
+**制約**
+
+- $p$ は素数
+- $p \leq 10^8$
+
+**計算量**
+
+- $O(p)$
+
+## comb
+
+```cpp
+Mint comb(int64_t n, int64_t k)
+```
+
+$_n C_k {\ } \pmod{p}$ を返します．
+
+**制約**
+
+- $0 \leq n, {\ } 0 \leq k$
+
+**計算量**
+
+- $O(\log n)$
+
+## private変数，private関数
+
+```cpp
+int p;
+vector<Mint> fact;
+pair<Mint, int64_t> extract(int64_t n);
+```
+
+- `p` : 法
+- `fact` : `fact[i]` は $i! \pmod{p}$
+- `extract` : $n = a \cdot p^e$ となる $a, e$ を返す関数
+
+
---
+++ kuro/smallmodfactorial.hpp
@@ -0,0 +1,41 @@
+#ifndef KURO_SMALLMODFACTORIAL_HPP
+#define KURO_SMALLMODFACTORIAL_HPP 1
+#include <kuro/modint>
+
+namespace kuro {
+
+// Small Mod Factorial
+template<typename Mint>
+struct SmallModFactorial {
+ public:
+  SmallModFactorial() : p(Mint::mod()), fact(p) {
+    fact[0] = 1;
+    for (int i = 1; i < p; ++i) {
+      fact[i] = fact[i-1]*i;
+    }
+  }
+  Mint comb(int64_t n, int64_t k) {
+    assert(0 <= n && 0 <= k);
+    if (n < k) return 0;
+    auto [a1, e1] = extract(n);
+    auto [a2, e2] = extract(k);
+    auto [a3, e3] = extract(n-k);
+    if (e1 > e2+e3) return 0;
+    return a1/(a2*a3);
+  }
+ private:
+  int p;
+  vector<Mint> fact;
+  pair<Mint, int64_t> extract(int64_t n) {
+    Mint a = 1;
+    int64_t e = 0;
+    while (n) {
+      a *= (n/p&1 ? -fact[n%p] : fact[n%p]);
+      e += (n /= p);
+    }
+    return {a, e};
+  }
+};
+
+} // namespace kuro
+#endif  // KURO_SMALLMODFACTORIAL_HPP
